4edf8928f1d5ae35c1c2c86c10df0057e1a77e54
Atabek <atabekarapov14@gmail.com>
2020-11-02 12:44:25 +0600

    Week3_Lecture3

---
+++ Lecture3.py
@@ -0,0 +1,66 @@
+# names = ('atai', 'nurbek', 'atabek', 'bektur', 'bakyt')
+# capitalized_names = []
+# for name in names:
+#     capitalized_names.append(name.capitalize())
+    
+
+
+
+# capitalized_names = [name.capitalize() for name in names]
+# print(capitalized_names)
+# capitalized_names ======>
+
+
+
+# nums = range(10)
+# print('Nums = ', nums)
+# square_nums = [num *num for num iin nums]
+# square_nums = ['num' + str(num) for num in nums]
+# print(list(nums))
+# print('squared_nums' squard jump
+
+
+# sers = ['Bakyt', 'Atabek', 'Amantai', 'Syimyk', 'The Best']
+# users_that_know_iterator = [user for user in users if user=='Amantai']
+# print(users_that_know_iteratoru)
+
+
+# nums = [1,2,3,4,5,6,-1,7,8-5, -4,3]
+# positive_nums = [num for num in nums if num>=0]
+# # print(positive_nums)
+# odd_nums = [num for num in nums if num % 2 !=0 and num>0]
+# print(odd_nums)
+
+
+# nums = range(100)
+# numss = [num for num in nums if num % 5==0 and num%3==0]
+# print(numss)
+
+
+
+# 5)
+# numss = [num * num2 or num2 in range(1.10)for num in range(1.10)]
+# print(numss)
+
+
+# 6)
+nums = [f'Even num {num}\n' if num % 2 == 0 else f'odd num {num}' for num in range(20)]
+print(nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
